505777d7524b9fde77e20d7ef4569ef9f23e987a
freezestanley <freezestanley@github.com>
2018-09-07 12:35:21 +0800

    xx

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,6 @@
+'use strict'
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const webpack = require('webpack')
